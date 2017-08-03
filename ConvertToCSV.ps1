55401395b708e7298af395de8f8e0d40416c021b
Raul Ramon <30698234+electronaut1984@users.noreply.github.com>
2017-08-03 16:29:00 -0500

    Covert To CSV
    Converts a source file into a CSV formatted file.

---
+++ ConvertToCSV.ps1
@@ -0,0 +1,13 @@
+﻿$sourcePath = $args[0]
+$destPath = $args[1]
+
+$excel = New-Object -ComObject Excel.Application
+$excel.Visible = $false
+$excel.DisplayAlerts = $false
+
+$workbook = $excel.Workbooks.Open($sourcePath)
+
+$xlCSV = 6
+$workbook.saveas($destPath,$xlCSV)
+
+$excel.Quit()
