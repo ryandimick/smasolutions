478af20a56324c9211d6755274743ac9a8ecc3bf
mpetrosky95 <30698257+mpetrosky95@users.noreply.github.com>
2017-08-03 10:52:49 -0500

    This is only an example
    This is only an example

---
+++ Mike_Example.ps1
@@ -0,0 +1,9 @@
+New-Item "C:\Human Resources\Incoming Files\Chicago.txt" -type file -force -value "I'm impressed with the people from Chicago. Hollywood is hype, New York is talk, Chicago is work. -Michael Douglas"
+
+New-Item "C:\Human Resources\Incoming Files\Houston.txt" -type file -force -value “Okay, Houston, we’ve had a problem here.  -John Swigert" 
+
+New-Item "C:\Human Resources\Incoming Files\Los Angeles.txt" -type file -force -value "I think the paparazzi might have chased me out of Los Angeles. -Nicolas Cage"
+
+New-Item "C:\Human Resources\Incoming Files\New York.txt" -type file -force -value "Anytime four New Yorkers get into a cab together without arguing, a bank robbery has just taken place. -Johnny Carson"
+
+New-Item "C:\Human Resources\Incoming Files\Philadelphia.txt" -type file -force -value "In Boston they ask, how much does he know? In New York, how much is he worth? In Philadelphia, who were his parents? ? Mark Twain"
