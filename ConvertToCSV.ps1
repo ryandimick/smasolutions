64f071c1a3275c633f49edf1c0ed67c7cb05c1d6
David Cornelius <dcornelius@SMAUSA.local>
2017-08-04 08:53:29 -0500

    Updated
    Made it more like the other ones

--- ConvertToCSV.ps1
+++ ConvertToCSV.ps1
@@ -1,13 +1,41 @@
-﻿$sourcePath = $args[0]
-$destPath = $args[1]
+﻿#	Syntax:
+#	SMAConvertToCVS.ps1 -sourcePath "C:Test\report\" -sourceFileName 12345 -destinationPath "\\opcon\report\ -destinationFileName tbal -sheetName OpCon
+#
+#	 
 
-$excel = New-Object -ComObject Excel.Application
-$excel.Visible = $false
-$excel.DisplayAlerts = $false
+param (
+    [string]$sourcePath,
+    [string]$sourceFileName,
+    [string]$destinationPath,
+    [string]$destinationFileName,
+    [string]$sheetName
+)
 
-$workbook = $excel.Workbooks.Open($sourcePath)
+try
+{
+    if (Test-Path $sourcePath And Test-Path $destinationPath)
+        {
+            echo "Error.  Folder path not reachable"
+            exit 7518
+        }
+    else 
+        {
+        $excel = New-Object -ComObject Excel.Application
+        $excel.Visible = $false
+        $excel.DisplayAlerts = $false
 
-$xlCSV = 6
-$workbook.saveas($destPath,$xlCSV)
+        $workbook = $excel.Workbooks.Open($sourcePath)
+        $WorkSheet = $WorkBook.sheets.item("$sheetName")
+        $xlCSV = 6
+        $workbook.saveas($destinationPath,$xlCSV)
 
 $excel.Quit()
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($objExcel)
+        }
+}
+
+catch [System.AccessViolationException]
+{
+	echo "Error.  User does not have access" + $Error[0]
+	exit 7507
+}
